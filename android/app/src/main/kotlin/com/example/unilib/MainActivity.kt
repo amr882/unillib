714a025a6d40d443545e2bc93197a55c5f1ff12e
Amr Fadel <fadelamr148asd@gmail.com>
2026-03-04 01:27:34 +0200

    first commite

---
+++ android/app/src/main/kotlin/com/example/unilib/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.unilib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/unilib.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UniLib extends StatelessWidget {
+  const UniLib({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
